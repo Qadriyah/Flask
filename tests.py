33b9ff87c83fdf61416e13096e6b5c9616c3390e
Qadriyah <b.alzawad@gmail.com>
2018-10-03 19:59:29 +0300

    Ch: Create test cases
    This commit adds tests for the Validator class
    [Delivers #160948967]

--- tests.py
+++ tests.py
@@ -1,9 +1,28 @@
 from unittest import TestCase
+from app.validation import validations
 
 
 class TestValidations(TestCase):
     def setUp(self):
-        pass
+        self.validator = validations.Validator()
+        self.data = {
+            "name": "Sekitoleko Baker",
+            "email": "becks@gmail.com",
+            "password": "mukunguB@01",
+            "password2": "mukunguB@01"
+        }
+
+    def test_validator_obj(self):
+        self.assertIsInstance(self.validator, validations.Validator)
+
+    def test_is_empty(self):
+        self.assertFalse(self.validator.is_empty(self.data))
+
+    def test_register_validation(self):
+        self.assertTrue(
+            self.validator.register_validation(self.data)["is_true"])
+        self.assertEqual(
+            len(self.validator.register_validation(self.data)["errors"]), 0)
 
 
 class TestModels(TestCase):
