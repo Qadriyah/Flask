0f763e7506b850d15065b0d690f5fdd09b09019d
Qadriyah <b.alzawad@gmail.com>
2018-10-06 08:45:40 +0300

    Added a test case for adding a user to the database

--- tests.py
+++ tests.py
@@ -1,39 +1,40 @@
 from unittest import TestCase
+
 from app.validation import validations
+from app.accounts import dao
+from app import app
+
+data = {
+    "name": "Sekitoleko Baker",
+    "email": "becks@gmail.com",
+    "password": "mukunguB@01",
+    "password2": "mukunguB@01"
+}
 
 
 class TestValidations(TestCase):
     def setUp(self):
         self.validator = validations.Validator()
-        self.data = {
-            "name": "Sekitoleko Baker",
-            "email": "becks@gmail.com",
-            "password": "mukunguB@01",
-            "password2": "mukunguB@01"
-        }
 
     def test_validator_obj(self):
         self.assertIsInstance(self.validator, validations.Validator)
 
     def test_is_empty(self):
-        self.assertFalse(self.validator.is_empty(self.data))
+        self.assertFalse(self.validator.is_empty(data))
 
     def test_register_input_validation(self):
-        self.assertTrue(
-            self.validator.register_input_validation(self.data)["is_true"])
         self.assertEqual(
-            len(self.validator.register_input_validation(self.data)["errors"]), 0)
+            len(self.validator.register_input_validation(data)["errors"]), 0)
 
     def test_validate_email(self):
-        self.assertTrue(self.validator.validate_email(self.data["email"]))
+        self.assertTrue(self.validator.validate_email(data["email"]))
 
     def test_validate_password(self):
-        self.assertTrue(self.validator.validate_password(
-            self.data["password"]))
+        self.assertTrue(self.validator.validate_password(data["password"]))
 
     def test_login_input_validation(self):
         self.assertTrue(
-            self.validator.login_input_validation(self.data)["is_true"])
+            self.validator.login_input_validation(data)["is_true"])
 
 
 class TestModels(TestCase):
@@ -43,7 +44,11 @@ class TestModels(TestCase):
 
 class TestDataAcess(TestCase):
     def setUp(self):
-        pass
+        self.dao_obj = dao.Account()
+
+    def test_add_account(self):
+        with app.app_context():
+            self.assertEqual(self.dao_obj.add_account(data)[1], 200)
 
 
 class TestAccountRoutes(TestCase):
